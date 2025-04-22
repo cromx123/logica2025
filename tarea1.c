b51ab605edac550c694106e63eef836ab5cca760
cromx123 <73903821+cromx123@users.noreply.github.com>
2025-04-22 19:20:16 -0400

    Create tarea1.c
    Se inicia el proyecto

---
+++ tarea1.c
@@ -0,0 +1,17 @@
+/***************************************************************************************
+ * 
+ * tarea1.c: Algoritmo de Satisfactibilidad 
+ * Programmer: Benjamin Caceres, Cristobal Gallardo
+ * Santiago de Chile, 26/05/2025
+ *
+ **************************************************************************************/
+
+ #include <stdio.h>
+ #include <stdlib.h>
+ #include <string.h>
+ 
+
+int main() {
+    
+    return 0;
+}
